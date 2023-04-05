da0cf40a9058b3814a3d39eca86ccd537cd51db7
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-05 17:39:45 +0200

    remove username

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 def lambda_handler(event, context):
-    response = requests.get('https://api.github.com/fdnieuwveldt')
+    response = requests.get('https://api.github.com')
     return {
         'statusCode': response.status_code,
         'body': response.content
