499f1e9c78fd8edbc6b3e3c24c161b58f627234b
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-05 17:46:55 +0200

    run build and deploy manually in console

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 def lambda_handler(event, context):
+    print("triggered with workflow dispatch")
     response = requests.get('https://api.github.com')
     return {
         'statusCode': response.status_code,
