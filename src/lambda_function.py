28b0bdcbec0dfcbd1a0870169217a6265a37077f
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-05 17:32:17 +0200

    add requests

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,9 @@
 import json
-
+import requests
 
 def lambda_handler(event, context):
+    response = requests.get('https://api.github.com/fdnieuwveldt')
     return {
-        'statusCode': 200,
-        'body': 'Hello, I was deployed from a github action'
+        'statusCode': response.status_code,
+        'body': response.content
     }
