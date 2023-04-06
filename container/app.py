fca8e7ba0292c5a7334c9760c2543583a41d6e67
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-06 22:13:49 +0200

    added app.py file

---
+++ container/app.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def lambda_handler(event, context):
+    print("triggered with workflow dispatch")
+    response = requests.get('https://api.github.com')
+    return {
+        'statusCode': response.status_code,
+        'body': response.content
+    }
