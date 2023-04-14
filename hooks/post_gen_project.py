559a5cb931cf5b19550038a66e059eb042f694d8
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-14 16:11:40 +0200

    cleanup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,5 @@
-import os
 import shutil
+from pathlib import Path
 
 
 project_name = "{{ cookiecutter.project_name }}"
@@ -9,34 +9,12 @@ all_pipelines = ["lambda_function", "custom_layer", "lambda_function_and_layer",
 
 def remove_dir(dir_list):
     for dir in dir_list:
-        cur_dir = os.path.abspath(os.path.curdir)
-        drop_dir = os.path.join(cur_dir, dir)
-        if os.path.exists(drop_dir):
+        drop_dir = Path.cwd() / dir
+        if drop_dir.exists():
             shutil.rmtree(drop_dir)
         else:
             print(f"Error: The directory '{drop_dir}' could not be removed")
 
 
 remove_list = [choice for choice in all_pipelines if choice != pipeline_choice]
-remove_dir(
-   remove_list
-)
-# from pathlib import Path
-
-
-# project_name = "{{ cookiecutter.project_name }}"
-# pipeline_choice = "{{ cookiecutter.pipeline_choice }}"
-# all_pipelines = ["lambda_function", "custom_layer", "lambda_function_and_layer", "container"]
-
-
-# def remove_dir(dir_list):
-#     for dir in dir_list:
-#         drop_dir = Path.cwd() / dir
-#         if drop_dir.is_dir():
-#             drop_dir.rmdir()
-#         else:
-#             print(f"Error: The directory '{drop_dir}' could not be removed")
-
-
-# remove_list = [choice for choice in all_pipelines if choice != pipeline_choice]
-# remove_dir(remove_list)
+remove_dir(remove_list)
