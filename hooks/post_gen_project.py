6723fbb6c798ef06deb8898fb01e3b897d4fa0bc
Fernando Nieuwveldt <fdnieuwveldt@gmail.com>
2023-04-14 16:03:31 +0200

    working changes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,18 +1,42 @@
 import os
 import shutil
-import sys
 
+
+project_name = "{{ cookiecutter.project_name }}"
 pipeline_choice = "{{ cookiecutter.pipeline_choice }}"
+all_pipelines = ["lambda_function", "custom_layer", "lambda_function_and_layer", "lambda_container"]
 
-if pipeline_choice not in ['custom_layer', 'lambda_container', 'lambda_function', 'lambda_function_and_layer']:
-    print(f"ERROR: Invalid pipeline choice '{pipeline_choice}'.")
-    sys.exit(1)
 
-source_directory = os.path.join('{{cookiecutter.pipeline_choice}}', pipeline_choice)
-destination_directory = '.'
+def remove_dir(dir_list):
+    for dir in dir_list:
+        cur_dir = os.path.abspath(os.path.curdir)
+        drop_dir = os.path.join(cur_dir, dir)
+        if os.path.exists(drop_dir):
+            shutil.rmtree(drop_dir)
+        else:
+            print(f"Error: The directory '{drop_dir}' could not be removed")
 
-for file in os.listdir(source_directory):
-    if file.endswith('.yml'):
-        shutil.copy2(os.path.join(source_directory, file), destination_directory)
 
-shutil.rmtree('{{cookiecutter.pipeline_choice}}')
+remove_list = [choice for choice in all_pipelines if choice != pipeline_choice]
+remove_dir(
+   remove_list
+)
+# from pathlib import Path
+
+
+# project_name = "{{ cookiecutter.project_name }}"
+# pipeline_choice = "{{ cookiecutter.pipeline_choice }}"
+# all_pipelines = ["lambda_function", "custom_layer", "lambda_function_and_layer", "container"]
+
+
+# def remove_dir(dir_list):
+#     for dir in dir_list:
+#         drop_dir = Path.cwd() / dir
+#         if drop_dir.is_dir():
+#             drop_dir.rmdir()
+#         else:
+#             print(f"Error: The directory '{drop_dir}' could not be removed")
+
+
+# remove_list = [choice for choice in all_pipelines if choice != pipeline_choice]
+# remove_dir(remove_list)
